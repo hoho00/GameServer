6b2d2567ed97b2724a6abeb787d286777f642e0f
hoho00 <jhoya963@gmail.com>
2024-12-04 22:27:54 +0900

    first commit

---
+++ src/main/java/com/watchGame/hoho00/GameServerApplication.java
@@ -0,0 +1,13 @@
+package com.watchGame.hoho00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/watchGame/hoho00/GameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watchGame.hoho00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
